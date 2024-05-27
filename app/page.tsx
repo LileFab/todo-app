b1740ec441b954bbdd33ab3e0d2390d4ae56442b
Fabien <backspeen@gmail.com>
2024-05-27 22:38:47 +0200

    ✨ Debut page accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import { Abril_Fatface} from "next/font/google";
+import { currentUser } from '@clerk/nextjs/server';
 
-export default function Home() {
+const abril = Abril_Fatface({ subsets: ["latin"], weight: ["400"] })
+
+export default async function Home() {
+
+  const user = await currentUser();
+
   return (
-    <>
-    <div>test</div>
-    </>
+    <div className="pt-36 w-screen text-center">
+      {!user &&
+        <h1 className= {`${abril.className} text-3xl`} >Merci de vous connecter afin d'accéder à vos tâches.</h1>
+      }
+      {user &&
+        <h1 className= {`${abril.className} text-5xl`} >&#9207; Vos tâches {user?.firstName}&#9207;</h1>
+      }
+      
+    </div>
     )
 }
