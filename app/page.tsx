ab2d031de4120bfd30bac5693cb9b1a758aab4b3
Fabien <backspeen@gmail.com>
2024-05-28 18:09:38 +0200

    ✨ Add modal to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import { Abril_Fatface } from "next/font/google";
 import { currentUser } from "@clerk/nextjs/server";
 import TaskCard from "./components/TaskCard";
 import TaskObject from "./interfaces/TaskInterface";
-import { getAllTasksByUserId, insertNewTask } from "./actions/taskActions";
-import TestButton from "./components/testCompoent/TestButton";
+import { getAllTasksByUserId } from "./actions/taskActions";
+import Modal from "./components/UI/Modal";
+import Button from "./components/UI/Button";
+import Link from "next/link";
+
+type SearchParamProps = {
+  searchParams: Record<string, string> | null | undefined;
+};
 
 const abril = Abril_Fatface({ subsets: ["latin"], weight: ["400"] });
 
@@ -17,11 +23,13 @@ const getData: () => Promise<[any, TaskObject[]]> = async () => {
   return [user, userTasks];
 };
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
   const [user, userTasks]: [any, TaskObject[]] = await getData();
+  const modalState = searchParams?.show;
 
   return (
-    <div className="pt-36 w-screen text-center">
+    <div className="pt-36 w-screen text-center z-10">
+      {modalState && <Modal />}
       {!user && (
         <h1 className={`${abril.className} text-3xl`}>
           Merci de vous connecter afin d'accéder à vos tâches.
@@ -33,7 +41,9 @@ export default async function Home() {
             &#9207; Vos tâches {user?.firstName}&#9207;
           </h1>
           {/* // faire une modal pour la création en use client dans le page.tsx de / */}
-          <TestButton text="Ajouter tâche de test" />
+          <Link href="/?show=true">
+            <Button text="Ouvrir la modal" />
+          </Link>
           <ul className="flex flex-col items-center">
             {userTasks &&
               userTasks.map((ut) => (
